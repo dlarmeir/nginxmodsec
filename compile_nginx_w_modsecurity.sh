098ccfb6ae136a799b769b1178eb144a406e7ca6
Dustin Larmeir <dustin@larmeir.com>
2016-10-23 10:34:22 -0400

    Working Copy - Tested on Ubunt 16.04 LTS

--- compile_nginx_w_modsecurity.sh
+++ compile_nginx_w_modsecurity.sh
@@ -8,37 +8,38 @@
 
 # Grab Buildtools and Deps 
 echo "********Installing Tools and Dependencies********"
-#sleep 2
-#apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
+sleep 2
+apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
 
 # Add The Official Nginx PPA repo with build sources
-#add-apt-repository ppa:nginx/stable -y
-#echo "deb-src http://ppa.launchpad.net/nginx/stable/ubuntu xenial main" >>/etc/apt/sources.list.d/nginx-ubuntu-stable-xenial.list
-#apt-get update
+add-apt-repository ppa:nginx/stable -y
+echo "deb-src http://ppa.launchpad.net/nginx/stable/ubuntu xenial main" >>/etc/apt/sources.list.d/nginx-ubuntu-stable-xenial.list
+apt-get update
 
 # grab build deps and source code
-#cd /usr/src/
-#apt-get build-dep nginx -y
-#apt-get source nginx
+cd /usr/src/
+apt-get build-dep nginx -y
+apt-get source nginx
 
 # Fetch Mod_Security Source
-#echo "********Fetching Mod_security Source Code********"
-#sleep 2
-#cd /usr/src/nginx-1.10.1/debian/modules
-#git clone https://github.com/SpiderLabs/ModSecurity.git
-#cd /usr/src/nginx-1.10.1/debian/modules/ModSecurity/
+echo "********Fetching Mod_security Source Code********"
+sleep 2
+cd /usr/src/nginx-1.10.1/debian/modules
+git clone https://github.com/SpiderLabs/ModSecurity.git
+cd /usr/src/nginx-1.10.1/debian/modules/ModSecurity/
 
 # Build ModSecurity stand alone module
 echo "********Compiling Mod_Security Source Code********"
-#sleep 2
-#./autogen.sh
-#./configure --enable-standalone-module
-#make
+sleep 2
+./autogen.sh
+./configure --enable-standalone-module
+make
 
 # Compile Nginx w/Mod_Security module
-sed -i '105i\\ 			--add-module=$(MODULESDIR)/ModSecurity/nginx \\' /usr/src/nginx-1.10.1/debian/rules
+cd /usr/src/nginx-1.10.1/
+sed -i '105i\\ 			--add-module=$(MODULESDIR)/ModSecurity/nginx/modsecurity \\' /usr/src/nginx-1.10.1/debian/rules
+dpkg-buildpackage -uc -b
 
-# Build Nginx
 
 echo "********Build Complete - Email dustin@larmeir.com if you encounter issues********"
 
