7e108307bf1fed738c4bc77d2e364a966a837fd2
Dustin Larmeir <dustin@larmeir.com>
2016-10-23 07:16:01 -0400

    Updates

--- compile_nginx_w_modsecurity.sh
+++ compile_nginx_w_modsecurity.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-#Grab Buildtools and Deps (Validated)
-#apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
 
-#Fetch Nginx Source (Validated)
-#cd /usr/src
-#git clone https://github.com/SpiderLabs/ModSecurity.git
-#cd /usr/src/ModSecurity/
+##############################
+# Dustin Larmeir 10/23/2016  #
+# Nginx w/Mod_Security Build #
+##############################
 
-# Build ModSecurity stand alone module (validated)
-#./autogen.sh
-#./configure --enable-standalone-module
-#make
+#Grab Buildtools and Deps 
+apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
+
+#Fetch Nginx Source
+cd /usr/src
+git clone https://github.com/SpiderLabs/ModSecurity.git
+cd /usr/src/ModSecurity/
+
+# Build ModSecurity stand alone module
+./autogen.sh
+./configure --enable-standalone-module
+make
 
 
