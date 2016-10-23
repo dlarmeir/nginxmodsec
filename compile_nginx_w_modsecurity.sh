ed8ed44d60dd8a67b090fd3b069c14e56e8640c9
root <root@waf.protectlayer7.com>
2016-10-23 07:12:54 -0400

    Adding initial scripty

---
+++ compile_nginx_w_modsecurity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Grab Buildtools and Deps (Validated)
+#apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
+
+#Fetch Nginx Source (Validated)
+#cd /usr/src
+#git clone https://github.com/SpiderLabs/ModSecurity.git
+#cd /usr/src/ModSecurity/
+
+# Build ModSecurity stand alone module (validated)
+#./autogen.sh
+#./configure --enable-standalone-module
+#make
+
+
