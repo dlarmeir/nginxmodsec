5642e90253cce7ab1d1e1991d7f4c88c5067500c
Dustin Larmeir <dustin@larmeir.com>
2016-10-24 19:09:40 -0400

    Cleaned up some code

--- nginxmodsec.sh
+++ nginxmodsec.sh
@@ -10,6 +10,7 @@
 echo "********Installing Tools and Dependencies********"
 sleep 2
 apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev -y
+
 # Add The Official Nginx PPA repo with build sources
 add-apt-repository ppa:nginx/stable -y
 echo "deb-src http://ppa.launchpad.net/nginx/stable/ubuntu xenial main" >>/etc/apt/sources.list.d/nginx-ubuntu-stable-xenial.list
@@ -56,7 +57,6 @@ echo "********Installing Mod_Security CRS********"
 sleep 2
 cd /etc/nginx/
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
-cd /etc/nginx/owasp-modsecurity-crs/
 
 echo "********Build Complete - Email dustin@larmeir.com if you encounter issues********"
 
